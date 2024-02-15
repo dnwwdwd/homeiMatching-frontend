56b9c5c6eaa6f08d4342573fb8462ff681c89511
C1own <1765393959@qq.com>
2024-02-15 19:22:37 +0800

    add 添加队伍详情页和更换队伍头像 add 前端响应拦截登录 add 完善注册页面，支持添加头像、标签、手机号等参数 add 用户介绍详情页 add 添加好友

--- components.d.ts
+++ components.d.ts
@@ -7,6 +7,7 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
+    FriendCardList: typeof import('./src/components/FriendCardList.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     TeamCardList: typeof import('./src/components/TeamCardList.vue')['default']
